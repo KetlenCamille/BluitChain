e9511750af74b8c5cc14c842421a3194619fe659
Mirella Lima <mirella.t.lima@hotmail.com>
2019-04-17 21:06:23 -0300

    UsuarioController (cadastrar usuario, listar usuarios), SingletonContext, CadastrarUsuario, Index (views pra teste)

---
+++ BlutChain/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using BlutChain.DAL;
+using BlutChain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlutChain.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private Context db = new Context();
+
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(UsuarioDAO.ListarUsuarios());
+        }
+
+        // GET: Usuario/Create
+        public ActionResult CadastrarUsuario()
+        {
+            if (TempData["Mensagem"] != null)
+            {
+                ModelState.AddModelError("", TempData["Mensagem"].ToString());
+            }
+            return View((Usuario)TempData["Usuario"]);
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarUsuario(Usuario usuario)
+        {
+            if (UsuarioDAO.CadastrarUsuario(usuario))
+            {
+                return RedirectToAction("Index", "Usuario");
+            }
+            ModelState.AddModelError("", "Esse usuário já existe!");
+            return View(usuario);
+
+        }
+    }
+}
---
+++ BlutChain/DAL/UsuarioDAO.cs
@@ -0,0 +1,50 @@
+﻿using BlutChain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlutChain.DAL
+{
+    public class UsuarioDAO
+    {
+        private static Context context = SingletonContext.GetInstance();
+
+
+        // Criar Usuário
+        public static bool CadastrarUsuario(Usuario usuario)
+        {
+            if (BuscarUsuarioPorCPF(usuario) == false)
+            {
+                context.Usuarios.Add(usuario);
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        // Buscar Usuário Por CPF
+        public static bool BuscarUsuarioPorCPF(Usuario usuario)
+        {
+            if (context.Usuarios.FirstOrDefault(x => x.CPFUsuario.Equals(usuario.CPFUsuario)) != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Listar Usuários
+        public static List<Usuario> ListarUsuarios()
+        {
+            return context.Usuarios.ToList();
+        }
+    }
+}
