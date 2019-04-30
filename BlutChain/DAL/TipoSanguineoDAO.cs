a5d188ebae7ab7e69a415005890437cb5bb19488
KetlenCamille <ketlen.rieper@gmail.com>
2019-04-30 10:11:06 -0300

    Criando TipoSanguineoDAO

---
+++ BlutChain/DAL/TipoSanguineoDAO.cs
@@ -0,0 +1,47 @@
+﻿using BlutChain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlutChain.DAL
+{
+    public class TipoSanguineoDAO
+    {
+        private static Context context = SingletonContext.GetInstance();
+
+        private static bool CadastrarTipoSanguineo(TipoSanguineo tipoSanguineo)
+        {
+            if(tipoSanguineo != null)
+            {
+                context.TipoSanguineos.Add(tipoSanguineo);
+                context.SaveChanges();
+            }
+            return false;
+        }
+
+        public static List<TipoSanguineo> ListarTodos()
+        {
+            return context.TipoSanguineos.ToList();
+        }
+
+        public static void AlterarTipoSanguineo (TipoSanguineo tipoSanguineo)
+        {
+            if(tipoSanguineo != null)
+            {
+                context.Entry(tipoSanguineo).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public static void ExcluirTipoSanguineo(TipoSanguineo tipoSanguineo)
+        {
+            context.TipoSanguineos.Remove(BuscarTipoSanguineoPorID(tipoSanguineo.IdTipoSanguineo));
+        }
+
+        public static TipoSanguineo BuscarTipoSanguineoPorID(int IDtipoSanguineo)
+        {
+            return context.TipoSanguineos.Find(IDtipoSanguineo);
+        }
+    }
+}
