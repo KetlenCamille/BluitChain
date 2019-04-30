d0fc9e218cc637b32b66056232a7f7a6fc31ea64
KetlenCamille <ketlen.rieper@gmail.com>
2019-04-30 11:14:10 -0300

    Criando a Index de TipoSanguineo

---
+++ BlutChain/Controllers/TipoSanguineoController.cs
@@ -0,0 +1,19 @@
+﻿using BlutChain.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlutChain.Controllers
+{
+    public class TipoSanguineoController : Controller
+    {
+        // GET: TipoSanguineo
+        public ActionResult Index()
+        {
+            ViewBag.Data = DateTime.Now;
+            return View(TipoSanguineoDAO.ListarTodos());
+        }
+    }
+}
