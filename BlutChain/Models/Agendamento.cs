b98539c73929c6452feebdb8bb14652e4ff8620c
Anna <anna.nester@gmail.com>
2019-05-13 10:03:34 -0300

    Alteraração Model e Context Agendamento

--- BlutChain/Models/Agendamento.cs
+++ BlutChain/Models/Agendamento.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BlutChain.Models
 {
+    [Table("Agendamento")]
     public class Agendamento
     {
+        [Key]
         public int IdAgendamento { get; set; }
 
         public DateTime DataAgendamento { get; set; }
