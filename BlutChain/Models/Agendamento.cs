7a5796988099169502f062fcf32719b025b64ea7
Anna <anna.nester@gmail.com>
2019-05-13 08:35:15 -0300

    Adicionado Model Agendamento

---
+++ BlutChain/Models/Agendamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlutChain.Models
+{
+    public class Agendamento
+    {
+        public int IdAgendamento { get; set; }
+
+        public DateTime DataAgendamento { get; set; }
+
+        public DateTime HorarioAgendamento { get; set; }
+
+        public Usuario IdUsuario { get; set; }
+
+        public Hemobanco IdHemobanco { get; set; }
+    }
+}
